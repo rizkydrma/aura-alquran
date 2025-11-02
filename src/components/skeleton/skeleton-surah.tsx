17c43832a3a03b731049244ef5f87116158c8db1
rizkydrma <rizky.darmarazak@gmail.com>
2025-11-03 06:28:36 +0700

    Add alignment and length props to SurahSkeleton component

--- src/components/skeleton/skeleton-surah.tsx
+++ src/components/skeleton/skeleton-surah.tsx
@@ -1,12 +1,19 @@
-export const SurahSkeleton = () => (
-    <div className="animate-pulse space-y-3">
-        <div className="flex items-start justify-between gap-4 rounded-xl bg-gray-200 p-4 dark:bg-neutral-800">
-            <div className="h-6 w-6 rounded bg-neutral-300 dark:bg-neutral-700" />
-            <div className="w-full space-y-2">
-                <div className="h-4 w-full rounded bg-neutral-300 dark:bg-neutral-700" />
-                <div className="h-3 w-full rounded bg-neutral-300 dark:bg-neutral-700" />
-                <div className="h-3 w-full rounded bg-neutral-300 dark:bg-neutral-700" />
-            </div>
+import { cn } from "@/lib/utils";
+
+export const SurahSkeleton = ({ align = "vertical", length = 3 }: { align?: "vertical" | "horizonal"; length?: number }) => {
+    return (
+        <div className={cn("grid gap-3", align == "vertical" ? "grid-cols-3" : "grid-cols-1")}>
+            {Array.from({ length }, (_, i) => (
+                <div className="animate-pulse space-y-3" key={i}>
+                    <div className="flex items-start justify-between gap-4 rounded-lg bg-gray-200 p-8 dark:bg-neutral-800">
+                        <div className="h-10 w-10 rounded bg-neutral-300 dark:bg-neutral-700" />
+                        <div className="w-full space-y-2">
+                            <div className="h-6 w-full rounded bg-neutral-300 dark:bg-neutral-700" />
+                            <div className="h-4 w-full rounded bg-neutral-300 dark:bg-neutral-700" />
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
-    </div>
-);
+    );
+};
